bbba6b533e0636cbd04ccf2abe2965aaf527c553
too-r <william@huddie.net>
2017-08-28 14:42:35 +0000

    Full rewrite of the ban command

--- src/commands/admin.rs
+++ src/commands/admin.rs
@@ -5,8 +5,19 @@ use std::path::Path;
 use super::config::Config;
 
 pub fn ban(msg: Message, srv_id: ServerId, discord: Discord, config: &Config) {
-    //The msg construct stores a list of mentions, the ban function takes the first mention and gets the id from there, meaning it is impossible to ban two users at once, you must run separate commands.
+    let config = config::get_config(); //Config object
+
+    //Take the id of the user we want to ban from the first mentioned user in the message.
     let user_id = msg.mentions[0].id;
+
+    let author_id = format!("{}", msg.author.id).parse::<u64>().unwrap(); //Parse id to string and back again
+
+    if config.admins.contains(author_id) {
+        discord.add_ban(srv_id, user_id);
+    } else {
+        let message = format!("{} You do not have permission to do that", user_id.mention());
+        discord.send_message(msg.channel_id, message, "", false);
+    }
 }
 
 pub fn kick(msg: Message, srv_id: ServerId, discord: Discord, config: &Config) {
