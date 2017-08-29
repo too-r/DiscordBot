6775a8f86d96e448424f3d17ef6b08959364848a
too-r <william@huddie.net>
2017-08-29 09:27:53 +0000

    Add a bit of logic to prevent admins from banning other admins

--- src/commands/admin.rs
+++ src/commands/admin.rs
@@ -12,11 +12,15 @@ pub fn ban(msg: Message, srv_id: ServerId, discord: Discord, config: &Config) {
 
     let author_id = format!("{}", msg.author.id).parse::<u64>().unwrap(); //Parse id to string and back again
 
-    if config.admins.contains(author_id) {
-        discord.add_ban(srv_id, user_id);
+    if config.admins.contains(user_id) {
+        discord.send_message(msg.channel_id, "Cannot ban a user who is set as an admin", "", false);
     } else {
-        let message = format!("{} You do not have permission to do that", user_id.mention());
-        discord.send_message(msg.channel_id, message, "", false);
+        if config.admins.contains(author_id) {
+            discord.add_ban(srv_id, user_id);
+        } else {
+            let message = format!("{} You do not have permission to do that", user_id.mention());
+            discord.send_message(msg.channel_id, message, "", false);
+        }
     }
 }
 
@@ -24,4 +28,15 @@ pub fn kick(msg: Message, srv_id: ServerId, discord: Discord, config: &Config) {
     let user_id = msg.mentions[0].id;
 
     let author_id = format!("{}", msg.author.id).parse::<u64>().unwrap();
+    
+    if config.admins.contains(user_id) {
+        discord.send_message(msg.channel_id, "Cannot kick a user who is set as an admin", "", false);
+    } else {
+        if config.admins.contains(author_id) {
+            discord.add_ban(srv_id, user_id);
+        } else {
+            let message = format!("{} You do not have permission to do that", user_id.mention());
+            discord.send_message(msg.channel_id, message, "", false);
+        }
+    }    
 }
