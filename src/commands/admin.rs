18231656857dc9925288d143241b817b1d455676
too-r <william@huddie.net>
2017-08-29 10:55:55 +0000

    Fix UserId -> u64 back and forth

--- src/commands/admin.rs
+++ src/commands/admin.rs
@@ -2,41 +2,45 @@ use discord::model::{Message, ServerId};
 use discord::Discord;
 use std::fs::File;
 use std::path::Path;
-use super::config::Config;
+use config::{self, Config};
 
 pub fn ban(msg: Message, srv_id: ServerId, discord: Discord, config: &Config) {
     let config = config::get_config(); //Config object
-
-    //Take the id of the user we want to ban from the first mentioned user in the message.
+    
     let user_id = msg.mentions[0].id;
+
+    //Make this a u64 just so we can check for it in the admins Vec.
+    let user_id_u64 = format!("{}", user_id).parse::<u64>().unwrap();
 
     let author_id = format!("{}", msg.author.id).parse::<u64>().unwrap(); //Parse id to string and back again
 
-    if config.admins.contains(user_id) {
+    if config.admins.admins.contains(&user_id_u64) {
         discord.send_message(msg.channel_id, "Cannot ban a user who is set as an admin", "", false);
     } else {
-        if config.admins.contains(author_id) {
-            discord.add_ban(srv_id, user_id);
+        if config.admins.admins.contains(&author_id) {
+            discord.add_ban(srv_id, user_id, 0);
         } else {
             let message = format!("{} You do not have permission to do that", user_id.mention());
-            discord.send_message(msg.channel_id, message, "", false);
+            discord.send_message(msg.channel_id, &message, "", false);
         }
     }
 }
 
 pub fn kick(msg: Message, srv_id: ServerId, discord: Discord, config: &Config) {
     let user_id = msg.mentions[0].id;
+    
+    let user_id_u64 = format!("{}", user_id).parse::<u64>().unwrap();
 
     let author_id = format!("{}", msg.author.id).parse::<u64>().unwrap();
     
-    if config.admins.contains(user_id) {
+    if config.admins.admins.contains(&user_id_u64) {
         discord.send_message(msg.channel_id, "Cannot kick a user who is set as an admin", "", false);
     } else {
-        if config.admins.contains(author_id) {
-            discord.add_ban(srv_id, user_id);
+        if config.admins.admins.contains(&author_id) {
+            discord.kick_member(srv_id, user_id);
         } else {
             let message = format!("{} You do not have permission to do that", user_id.mention());
-            discord.send_message(msg.channel_id, message, "", false);
+            discord.send_message(msg.channel_id, &message, "", false);
         }
     }    
 }
