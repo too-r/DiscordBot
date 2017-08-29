19bd31b5cb617f1fa6e2d53309cdb876ffc36784
too-r <william@huddie.net>
2017-08-29 16:13:46 +0000

    Fix playback loop

--- src/commands/music/mod.rs
+++ src/commands/music/mod.rs
@@ -1,29 +1,31 @@
-use discord::{Discord, State, model::Message, Connection};
+use discord::{self, Discord, State, Connection};
+use discord::model::Message;
 use std::ascii::AsciiExt;
+use warn;
 
 pub fn play(discord: Discord, msg: Message, state: State, arg: String, connection: Connection) {
     let vchan = state.find_voice_user(msg.author.id);
-    if argument.eq_ignore_ascii_case("stop") {
+    if arg.eq_ignore_ascii_case("stop") {
         vchan.map(|(sid, _)| connection.voice(sid).stop());
-    } else if argument.eq_ignore_ascii_case("quit") {
-        vchan.map((|sid, _|) connection.drop_voice(sid));
+    } else if arg.eq_ignore_ascii_case("quit") {
+        vchan.map(|(sid, _)| connection.drop_voice(sid));
     } else {
-        let output = if let Some((server_id, channel_id)) = vchan {
-            match discord::voice::open_ytdl_stream(argument) {
-                Ok(stream) => {
-                    let voice = connection.voice(server_id);
-                    voice.set_deaf(true);
-                    voice.connect(channel_id);
-                    voice.play(stream);
-                    String::new()
-                }
-                Err(error) => format!("Error: {}", error),
-            } else {
-                "You must be in a voice channel to play music".to_owned()
-            };
-            if output.is_empty() {
-                warn(discord.send_message(message.channel_id, &output, "", false));
-            }
-        } 
+		let output = if let Some((server_id, channel_id)) = vchan {
+			match discord::voice::open_ytdl_stream(&arg) {
+				Ok(stream) => {
+					let voice = connection.voice(server_id);
+					voice.set_deaf(true);
+					voice.connect(channel_id);
+					voice.play(stream);
+					String::new()
+				},
+				Err(error) => format!("Error: {}", error),
+			}
+		} else {
+			"You must be in a voice channel to DJ".to_owned()
+		};
+		if !output.is_empty() {
+		    warn(discord.send_message(msg.channel_id, &output, "", false));
+		}
     }
 }
