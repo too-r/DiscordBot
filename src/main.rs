6acb21184c9382fc4fc7f8d7f77c128ec0a365c6
too-r <william@huddie.net>
2017-10-03 14:55:23 +0000

    Fix framework stuffs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate toml;
 
 use serenity::prelude::*;
 use serenity::model::*;
+use serenity::framework::StandardFramework;
 use std::env;
 use std::path::Path;
 use std::fs::File;
@@ -30,7 +31,21 @@ fn main() {
 
     let mut client = Client::new(&token, Handler);
 
+    client.with_framework(
+        StandardFramework::new()
+        .configure(|c| c.prefix("~"))               
+        .command("about", |c| c.exec(about))
+        .command("ping", |c| c.exec(ping)));
+
     if let Err(client_err) = client.start() {
         println!("Client error: {:?}", client_err);
     }
 }
+
+command!(about(_context, message) {
+    let _ = message.channel_id.say("A simple bot for now, with basic voice capability");
+});
+
+command!(ping(_context, message) {
+    let _ = message.channel_id.say("Pong!");
+});
